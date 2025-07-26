d028a184743a23052e8b23067dc77dc1c6592457
ACCTEMP-commits <shefojemy@gmail.com>
2025-07-26 03:02:43 +0200

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,8 +2,8 @@
 
 import { useState } from 'react';
 import Link from "next/link";
-import { Button, buttonVariants } from "./ui/button";
-import { BookIcon, CommandIcon, FileIcon, FilmIcon, HeartIcon, Link2Icon, LinkIcon, MenuIcon, TriangleIcon } from "lucide-react";
+import { buttonVariants } from "./ui/button";
+import { CommandIcon, LinkIcon, TriangleIcon } from "lucide-react";
 import toast from "react-hot-toast";
 
 export function Footer() {
@@ -29,7 +29,7 @@ export function Footer() {
 }
 
 export function FooterButtons() {
-  const [copied, setCopied] = useState(false);
+  const [ setCopied] = useState(false);
 
   const handleShare = async () => {
     const url = window.location.href;
